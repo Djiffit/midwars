3c2d72ea847719466dc89435a180038cbad052a4
Lindström Olli-Pekka <oplindst@da5-cs-b221-28.helsinki.fi>
2016-05-17 12:53:59 +0300

    nymphora

--- teams/template_team/heroes/nymphora_main.lua
+++ teams/template_team/heroes/nymphora_main.lua
@@ -14,7 +14,7 @@ object.bAttackCommands = true
 object.bAbilityCommands = true
 object.bOtherCommands = true
 
-object.bReportBehavior = false
+object.bReportBehavior = true
 object.bDebugUtility = false
 object.bDebugExecute = false
 
@@ -49,6 +49,15 @@ BotEcho('loading nymphora_main...')
 object.heroName = 'Hero_Fairy'
 
 --------------------------------
+-- Items
+--------------------------------
+
+behaviorLib.StartingItems = {"Item_ManaPotion", "Item_HealthPotion", "Item_EnhancedMarchers", "Item_HomecomingStone"}
+behaviorLib.EarlyItems = {"2 Item_Strength5", "Item_HomecomingStone"}
+behaviorLib.MidItems = {"Item_Shield2", "Item_HomecomingStone"}
+behaviorLib.LateItems = {"Item_Morph", "Item_Silence"}
+
+--------------------------------
 -- Lanes
 --------------------------------
 core.tLanePreferences = {Jungle = 0, Mid = 0, ShortSolo = 0, LongSolo = 0, ShortSupport = 5, LongSupport = 5, ShortCarry = 0, LongCarry = 0}
@@ -80,12 +89,20 @@ function object:SkillBuild()
 
   if skills.ulti:CanLevelUp() then
     skills.ulti:LevelUp()
+  elseif skills.heal:CanLevelUp() and skills.mana.GetLevel() > 0 and skills.stun.GetLevel() > 0 then
+    skills.heal:LevelUp()
+  elseif skills.mana:CanLevelUp() and skills.stun.GetLevel() > 0 and skills.mana:GetLevel() < skills.heal.GetLevel() then
+    skills.mana:LevelUp()
+  elseif skills.heal:GetLevel() == 0 then
+    skills.heal:LevelUp()
+  elseif skills.mana:GetLevel() == 0 then
+    skills.mana:LevelUp()
+  elseif skills.stun:CanLevelUp() then
+    skills.stun:LevelUp()
   elseif skills.heal:CanLevelUp() then
     skills.heal:LevelUp()
   elseif skills.mana:CanLevelUp() then
     skills.mana:LevelUp()
-  elseif skills.stun:CanLevelUp() then
-    skills.stun:LevelUp()
   else
     skills.attributeBoost:LevelUp()
   end
@@ -119,5 +136,111 @@ end
 -- override combat event trigger function.
 object.oncombateventOld = object.oncombatevent
 object.oncombatevent = object.oncombateventOverride
+
+local function HealUtility(botBrain)
+  local heal = skills.heal
+  local me = core.unitSelf
+  if heal and heal:CanActivate() then
+    if me:GetHealthPercent() < 0.5 and me:GetManaPercent() > 0.5 then
+      return 60
+    end
+  end
+  return 0
+end
+
+local function HealExecute(botBrain)
+  local heal = skills.heal
+  local selfPos = core.unitSelf:GetPosition()
+  if heal and heal:CanActivate() then
+    return core.OrderAbilityPosition(botBrain, heal, selfPos)
+  end
+  return false
+end
+local HealBehavior = {}
+HealBehavior["Utility"] = HealUtility
+HealBehavior["Execute"] = HealExecute
+HealBehavior["Name"] = "Healing"
+tinsert(behaviorLib.tBehaviors, HealBehavior)
+-- Tähtää vihollisiin, mene itse alueelle
+
+local function ManaUtility(botBrain)
+  local mana = skills.mana
+  local me = core.unitSelf
+  local myPos = me:GetPosition()
+  if mana and mana:CanActivate() then
+    local enemies = core.CopyTable(core.localUnits["EnemyHeroes"])
+    for enemy in enemies do
+      local enemyPos = enemy:GetPosition()
+      local enemyRange = enemy:GetAttackRange()
+      local distanceEnemy = Vector3.Distance2DSq(myPos, enemyPos)
+      if distanceEnemy < 2 * enemyRange then
+        Echo("Too close to enemy")
+        return 0
+      end
+    end
+    return 60
+  end
+  return 0
+end
+
+local function ManaExecute(botBrain)
+  local mana = skills.mana
+  local selfPos = core.unitSelf:GetPosition()
+  if mana and mana:CanActivate() then
+    return core.OrderAbilityPosition(botBrain, mana, selfPos)
+  end
+  return false
+end
+local ManaBehavior = {}
+ManaBehavior["Utility"] = ManaUtility
+ManaBehavior["Execute"] = ManaExecute
+ManaBehavior["Name"] = "Mana"
+tinsert(behaviorLib.tBehaviors, ManaBehavior)
+
+local stunTarget = nil
+local function StunUtility(botBrain)
+  local stun = skills.stun
+  local me = core.unitSelf
+  local myPos = me:GetPosition()
+  if stun and stun:CanActivate() then
+    local allies = core.CopyTable(core.localUnits["AllyUnits"])
+    local enemies = core.CopyTable(core.localUnits["EnemyHeroes"])
+    for enemy in enemies do
+      local enemyPos = enemy:GetPosition()
+      local distanceEnemy = Vector3.Distance2DSq(myPos, enemyPos)
+      if distanceEnemy < 450 then
+        for ally in allies do
+          local closeAllyCount = 0
+          local allyPos = ally:GetPosition()
+          local allyDistanceFromEnemy = Vector3.Distance2DSq(enemyPos, allyPos)
+          if allyDistanceFromEnemy < 100 then
+            closeAllyCount += 1
+          end
+          if closeAllyCount >= 5
+            stunTarget = enemy
+            return 60
+          end
+        end
+        Echo("No close allies")
+      end
+    end
+    return 0
+  end
+  return 0
+end
+
+local function StunExecute(botBrain)
+  local stun = skills.stun
+  local selfPos = core.unitSelf:GetPosition()
+  if stun and stun:CanActivate() and stunTarget then
+    return core.OrderAbilityPosition(botBrain, stun, stunTarget)
+  end
+  return false
+end
+local StunBehavior = {}
+StunBehavior["Utility"] = StunUtility
+StunBehavior["Execute"] = StunExecute
+StunBehavior["Name"] = "Stun"
+tinsert(behaviorLib.tBehaviors, StunBehavior)
 
 BotEcho('finished loading nymphora_main')
