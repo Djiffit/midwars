9641bc7cf79cbcb91ef8903e306e6b260e58f482
Konsta Kutvonen <djiffit@gmail.com>
2016-05-18 15:58:01 +0300

    Update valkyrie_main.lua

--- teams/template_team/heroes/valkyrie_main.lua
+++ teams/template_team/heroes/valkyrie_main.lua
@@ -14,7 +14,7 @@ object.bAttackCommands = true
 object.bAbilityCommands = true
 object.bOtherCommands = true
 
-object.bReportBehavior = true
+object.bReportBehavior = false
 object.bDebugUtility = false
 object.bDebugExecute = false
 
@@ -29,6 +29,7 @@ object.behaviorLib = {}
 object.skills = {}
 object.nTime = 0
 
+
 runfile "bots/core.lua"
 runfile "bots/botbraincore.lua"
 runfile "bots/eventsLib.lua"
@@ -36,8 +37,6 @@ runfile "bots/metadata.lua"
 runfile "bots/behaviorLib.lua"
 
 local core, eventsLib, behaviorLib, metadata, skills = object.core, object.eventsLib, object.behaviorLib, object.metadata, object.skills
-behaviorLib.LaneItems  = 
-			{"Item_Bottle", "Item_Marchers", "Item_Pierce", "Item_Immunity"}
 local print, ipairs, pairs, string, table, next, type, tinsert, tremove, tsort, format, tostring, tonumber, strfind, strsub
   = _G.print, _G.ipairs, _G.pairs, _G.string, _G.table, _G.next, _G.type, _G.table.insert, _G.table.remove, _G.table.sort, _G.string.format, _G.tostring, _G.tonumber, _G.string.find, _G.string.sub
 local ceil, floor, pi, tan, atan, atan2, abs, cos, sin, acos, max, random
@@ -49,27 +48,14 @@ local Clamp = core.Clamp
 BotEcho('loading valkyrie_main...')
 
 object.heroName = 'Hero_Valkyrie'
-
-object.nCallUp = 25
-object.nArrowUp = 30
-object.nLeapUp = 30
-object.nUltUp = 15
-object.nEnergUp = 10
-
--- Bonus agression points that are applied to the bot upon successfully using a skill/item
-object.nCallUse = 35
-object.nArrowUse = 40
-object.nLeapUse = 10
-object.nUltUse = 0
-object.nEnergUse = 10
-
--- Thresholds of aggression the bot must reach to use these abilities
-object.nCallThreshold = 30
-object.nArrowThreshold = 25
-object.nLeapThreshold = 95
-object.nUltThreshold = 30
-object.nEnergThreshold = 35
-object.nNullThreshold = 45
+behaviorLib.StartingItems  = 
+			{"Item_Bottle"}
+behaviorLib.LaneItems  = 
+			{"Item_PowerSupply", "Item_Steamboots", "Item_HomecomingStone"}
+behaviorLib.MidItems  = 
+			{"Item_Soulscream", "Item_Energizer", "Item_Lightbrand"}
+behaviorLib.LateItems  = 
+			{"Item_Dawnbringer", "Item_ManaBurn1 2", "Item_Weapon3", "Item_Evasion"}
 
 -- tavarat
 
@@ -158,80 +144,90 @@ object.oncombateventOld = object.oncombatevent
 object.oncombatevent = object.oncombateventOverride
 
 -- CHECK IF SOMETHING IS BLOCKING THE JAVELIN ( ARROW ) 
-function NoObstructions(me, enemy, obstructions, size)
+function NoObstructionsMy(me, enemy, obstructions, size)
 local bDebugLines = true
 	local path = Vector3.Distance2DSq(me, enemy)
+	local blocking = 0
 	for _, blocker in pairs(obstructions) do
-		if blocker and Vector3.Distance2DSq(me, blocker) < path then
-			if Vector3.Distance2DSq(core.unitSelf, vecTargetPosition) < (range*range) and NoObstructions(unitSelf:GetPosition(), vecTargetPosition, units, 110) then
-				local point = core.GetFurthestPointOnLine(me:GetPosition(), me, enemy)
-				local blockerRadius = blocker:GetBoundsRadius() * sqrt(2)
-				local blockerradiussq = blockerRadius * blockerRadius
-			
-				if Vector3.Distance2DSq(candidate:GetPosition(), point) <= 1500+ blockerradiussq then
-					return false
-				end 
-			end
-
-		return true
+		if blocker and blocker:GetPosition() ~= enemy and Vector3.Distance2DSq(me, blocker:GetPosition()) < path then
+			BotEcho(blocking)
+			local point = core.GetFurthestPointOnLine(blocker:GetPosition(), me, enemy)
+			local blockerRadius = blocker:GetBoundsRadius()
+			local blockerradiussq = blockerRadius * blockerRadius
+			if Vector3.Distance2DSq(blocker:GetPosition(), point) <= 2000 + blockerradiussq then
+				blocking = blocking + 1
+				return false
+			end 
 		end
 	end
+	BotEcho("oy m8!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+	return true
+end
+
+local function NoObstructions(pos1, pos2)
+  core.DrawDebugLine(pos1, pos2, "yellow")
+  local tEnemies = core.CopyTable(core.localUnits["EnemyCreeps"])
+  local distanceLine = Vector3.Distance2DSq(pos1, pos2)
+  local x1, x2, y1, y2 = pos1.x, pos2.x, pos1.y, pos2.y
+  local spaceBetween = 255 * 255
+  for _, creep in pairs(tEnemies) do
+    local posCreep = creep:GetPosition()
+    local x3, y3 = posCreep.x, posCreep.y
+    local calc = x1*y2 - x2*y1 + x2*y3 - x3*y2 + x3*y1 - x1*y3
+    local calc2 = calc * calc
+    local actual = calc2 / distanceLine
+    if actual < spaceBetween then
+      core.DrawXPosition(posCreep, "red", 25)
+      return false
+    end
+  end
+  core.DrawDebugLine(pos1, pos2, "green")
+  return true
 end
 	
-local function HarassHeroExecuteOverride(botBrain)
+-------------999999999---------------99999999------------	
+---------------	-- JA VE LI N AKA AARROW----------------
+------------------------999999999999999-------------9999--
+function JavelinUtility(botBrain)
 	local unitTarget = behaviorLib.heroTarget
-	if unitTarget == nil then
-		return object.harassExecuteOld(botBrain)
-	end
-	
-	
-	if core.CanSeeUnit(botBrain, unitTarget) and skills.javelin:CanActivate() and unitTarget.storedPosition and unitTarget.lastStoredPosition then
+	local unitSelf = core.unitSelf
+	local bActionTaken = false
+	if unitTarget and (unitSelf:GetLevel() > 3 or unitSelf:GetManaPercent() * 100 > 90) and core.CanSeeUnit(botBrain, unitTarget) and skills.javelin:CanActivate() and unitTarget.storedPosition and unitTarget.lastStoredPosition then
 		range = skills.javelin:GetRange()
 		local targetspeed = unitTarget.storedPosition - unitTarget.lastStoredPosition
-		local vecTargetPosition = unitTarget:GetPosition() + targetspeed
-		local units = core.CopyTable(core.localUnits["AllyCreeps"])
-		for key, unit in pairs(core.localUnits["EnemyCreeps"]) do
-			units[key] = unit
+		local targetposition = unitTarget:GetPosition()
+		local units = core.CopyTable(core.localUnits["EnemyCreeps"])
+		if NoObstructions(unitSelf:GetPosition(), targetposition) then
+			return 100
 		end
-		for key, unit in pairs(core.localUnits["AllyHeroes"]) do
-			units[key] = unit
-		end
-		
-	
-	return object.harassExecuteOld(botBrain)
-end
+	end
+	return 1
 end
 
-object.harassExecuteOld = behaviorLib.HarassHeroBehavior["Execute"]
-behaviorLib.HarassHeroBehavior["Execute"] = HarassHeroExecuteOverride
-
-
-
-
-function TurnTheShip(botBrain, vecHero, vecEnemy, nType)
+function JavelinExecute(botBrain)
+	local javelin = skills.javelin
+	local unitTarget = behaviorLib.heroTarget
 	local unitSelf = core.unitSelf
-	local nDelay = 300
-	local vecNegEnemy = vecEnemy * -1
-	local bActionTaken = false
-	local nCurTime = 0
-	object.nTime = HoN.GetGameTime()
-	
-	if nType == 0 then
-		bActionTaken = core.OrderMoveToUnitClamp(botBrain, unitSelf, vecEnemy)
-	end
-	if nType == 1 then
-		bActionTaken = core.OrderMoveToUnitClamp(botBrain, unitSelf, vecNegEnemy)
-	end
-	
-	for nCurTime = HoN.GetGameTime(), object.nTime + 500 do
-		if nCurTime - object.nTime >= nDelay then
-			return true
+	if javelin:CanActivate() then
+		if unitTarget and core.CanSeeUnit(botBrain, unitTarget) and skills.javelin:CanActivate() and unitTarget.storedPosition and unitTarget.lastStoredPosition then
+			range = skills.javelin:GetRange()
+			local targetspeed = unitTarget.storedPosition - unitTarget.lastStoredPosition
+			local targetposition = unitTarget:GetPosition() + targetspeed
+			local units = core.CopyTable(core.localUnits["EnemyCreeps"])
+			if NoObstructions(unitSelf:GetPosition(), targetposition) then
+				local bActionTaken = core.OrderAbilityPosition(botBrain, javelin, targetposition)
+				core.OrderAbilityPosition(botBrain, javelin, targetposition)
+			end
 		end
-		nCurTime = HoN.GetGameTime()
 	end
-	
-	return false
 end
+
+
+JavelinBehavior = {}
+JavelinBehavior["Utility"] = JavelinUtility
+JavelinBehavior["Execute"] = JavelinExecute
+JavelinBehavior["Name"] = "Javelin"
+tinsert(behaviorLib.tBehaviors, JavelinBehavior)
 
 
 
@@ -244,6 +240,7 @@ end
 function EnemiesNear(herolocation, enemies, range, style)
 	local dangerarea = range
 	local howmanyenemies = 0
+	local lowhealth = 0
 	for index, danger in pairs(enemies) do
 		local wheredanger = danger:GetPosition()
 		if wheredanger then
@@ -258,44 +255,80 @@ function EnemiesNear(herolocation, enemies, range, style)
 	end
 	if howmanyenemies >= 1 and style == 1 then
 		return true
-	elseif howmanyenemies >= 3 then
+	elseif howmanyenemies >= 4 then
 		return true
 	end
 	return false
 end
 
+-- HARASSMENT REEEEEEEEEEEEEEEEEEEEEEEEEE
+
+local function HarassHeroExecuteOverride(botBrain)
+	local unitTarget = behaviorLib.heroTarget
+	
+	if unitTarget == nil then
+		return object.harassExecuteOld(botBrain) --Target is invalid, move on to the next behavior
+	end
+	
+	local unitSelf = core.unitSelf
+	local attackrange = core.GetAbsoluteAttackRangeToUnit(unitSelf, unitTarget)
+	local extrarange = core.GetExtraRange(unitSelf)
+	local bActionTaken = false
+	local targetstunned = unitTarget:IsStunned()
+
+	if core.CanSeeUnit(botBrain, unitTarget) then
+		if targetstunned then
+			BotEcho("COMBO???? 111111111111111111111111")
+			if skills.leap:CanActivate() and skills.call:CanActivate() and core.unitSelf:GetLevel() > 5 and unitSelf:GetManaPercent() then
+				local range = skills.leap:GetRange() * skills.leap:GetRange() + 250 * 250
+				if Vector3.Distance2DSq(unitSelf:GetPosition(), unitTarget:GetPosition()) < range then
+					bActionTaken = core.OrderAbility(botBrain, skills.leap)
+				end
+			end
+		end
+		if not bActionTaken and skills.call:CanActivate() then
+		
+			bActionTaken = core.OrderAbility(botBrain, skills.call)
+			BotEcho("COMBO???? 222222222222222222222222")
+		end
+		if not bActionTaken and skills.call:CanActivate() and core.unitSelf:GetHealthPercent() > unitTarget:GetHealthPercent() * 2 and Vector3.Distance2DSq(unitSelf:GetPosition(), unitTarget:GetPosition()) < 800*800 then
+			bActionTaken = core.OrderAbility(botBrain, skills.leap)
+			BotEcho("COMBO???? 3333333333333333333333333")
+		end
+	end
+	if not bActionTaken then
+		return object.harassExecuteOld(botBrain)
+	end
+end
+-- overload the behaviour stock function with custom 
+object.harassExecuteOld = behaviorLib.HarassHeroBehavior["Execute"]
+behaviorLib.HarassHeroBehavior["Execute"] = HarassHeroExecuteOverride
+
+
+
 --------------------------------------------------------------
 -- Prism
 --------------------------------------------------------------
 function PrismUtility(botBrain)
-	local nUtility = 0
 	local ulti = skills.ulti
-	local nBadIdea = 1
-
 	if ulti:CanActivate() then
 		local allies = HoN.GetHeroes(core.myTeam)
 		for _, health in pairs(allies) do
 			local low = health:GetHealthPercent()
-			if low <= 0.2 and low > 0 then
+			if low <= 0.245901 and low > 0 then
 				local allyposition = health:GetPosition()
 				local enemies = HoN.GetHeroes(core.enemyTeam)
 				if EnemiesNear(allyposition, enemies, 700, 0) then
-					nUtility = 100
-					return nUtility
+					return 100
 				end
 			end
 		end
 	end
-	
-	
-	
-	nUtility = nBadIdea
-	return nUtility
+	return 0
 end
 
 function PrismExecute(botBrain)
 	local ulti = skills.ulti
-	
 	if ulti:CanActivate() then
 		core.OrderAbility(botBrain, ulti)
 	end
@@ -320,16 +353,55 @@ function CallUtility(botBrain)
 		local heroesNearby = HoN.GetHeroes(core.enemyTeam)
 		local creepsNearby = core.CopyTable(core.localUnits["EnemyCreeps"])
 		local currentMana = unitSelf:GetManaPercent()
-		if currentMana * 100 > 60 then
+		if currentMana * 100 > 66.6 then
 			if EnemiesNear(unitSelf:GetPosition(), creepsNearby, range, 2) then
-				return 100
+				return 35
 			end
 		end
 		if EnemiesNear(unitSelf:GetPosition(), heroesNearby, range, 1) then
-			return 100
+			return 35
 		end
 	end
 	return 1
+end
+
+function CallExecute(botBrain)
+	local call = skills.call
+	if call:CanActivate() then
+		core.OrderAbility(botBrain, call)
+	end
+end
+
+CallBehavior = {}
+CallBehavior["Utility"] = CallUtility
+CallBehavior["Execute"] = CallExecute
+CallBehavior["Name"] = "Call"
+tinsert(behaviorLib.tBehaviors, CallBehavior)
+
+
+
+--------------------------------------------------------------
+-- Leeaaappeerr
+--------------------------------------------------------------
+
+function CallUtility(botBrain)
+	local call = skills.call
+	local unitSelf = core.unitSelf
+	local range = 650
+	if call:CanActivate() then
+		local heroesNearby = HoN.GetHeroes(core.enemyTeam)
+		local creepsNearby = core.CopyTable(core.localUnits["EnemyCreeps"])
+		local currentMana = unitSelf:GetManaPercent()
+		if currentMana * 100 > 66.6 then
+			if EnemiesNear(unitSelf:GetPosition(), creepsNearby, range, 2) then
+				return 35
+			end
+		end
+		if EnemiesNear(unitSelf:GetPosition(), heroesNearby, range, 1) then
+			return 50
+		end
+	end
+	return 0
 end
 
 function CallExecute(botBrain)
@@ -359,13 +431,14 @@ local function RetreatFromThreatExecuteOverride(botBrain)
 	
 	if not bActionTaken then
 		local heroes = HoN.GetHeroes(core.enemyTeam)
-		if EnemiesNear(unitSelf:GetPosition(), heroes, 400, 0) and leap:CanActivate() then
+		if core.GetLastBehaviorName(botBrain) == "RetreatFromThreat" and EnemiesNear(unitSelf:GetPosition(), heroes, 400, 0) and leap:CanActivate() then
 			for index, enemy in pairs(heroes) do
 				if leap:CanActivate() then
+					BotEcho("JUmp away now go m ister mister ")
 					bActionTaken = core.OrderAbility(botBrain, leap)
 				end
 			end
-		else
+		elseif core.GetLastBehaviorName(botBrain) == "RetreatFromThreat" then
 			if arrow and arrow:CanActivate() then
 				local dangerdistance = 0
 				heroes = HoN.GetHeroes(core.enemyTeam)
@@ -373,11 +446,12 @@ local function RetreatFromThreatExecuteOverride(botBrain)
 					for index, enemy in pairs(heroes) do
 						if enemy:GetPosition() then
 							dangerdistance = Vector3.Distance2DSq(unitSelf:GetPosition(), enemy:GetPosition())
-							if TurnTheShip(botBrain, unitSelf:GetPosition(), enemy:GetPosition(), 1) then
+							local units = core.CopyTable(core.localUnits["EnemyCreeps"])
+							if NoObstructions(unitSelf:GetPosition(), enemy:GetPosition()) then
+								BotEcho("ARROW???????????????????????????????????????????????????????????????")
 								bActionTaken = core.OrderAbilityPosition(botBrain, arrow, enemy:GetPosition())
 							end
 						end
-						BotEcho("ARROW???????????????????????????????????????????????????????????????")
 					end
 				end
 			end
