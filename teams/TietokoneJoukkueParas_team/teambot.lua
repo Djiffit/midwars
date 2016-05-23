db58a7bdd51d2db94b62194a76e305583d289ec9
Simo Korkolainen <simo.korkolainen@gmail.com>
2016-05-23 13:19:04 +0300

    Update teambot.lua

--- teams/TietokoneJoukkueParas_team/teambot.lua
+++ teams/TietokoneJoukkueParas_team/teambot.lua
@@ -18,3 +18,75 @@ function object:onthinkOverride(tGameVariables)
 end
 object.onthinkOld = object.onthink
 object.onthink = object.onthinkOverride
+
+
+teamHeroTarget = nil
+
+
+function object:GetTeamTarget()
+	if teamHeroTarget and teamHeroTarget:IsValid() then
+		if self:CanSeeUnit(teamHeroTarget) then
+			return seft:GetMemoryUnit(teamHeroTarget)
+		end
+
+		teamHeroTarget = nil
+	end
+
+	return nil
+
+end
+
+
+function object:SetTeamTarget(target)
+
+	local oldTarget = self:GetTeamTarget()
+
+	if not oldTarget then
+		teamHeroTarget = target
+		return
+	end
+
+	
+	teamHeroTarget = BetterTarget(oldTarget, target)
+
+end
+
+
+
+local function BetterTarget(first, second) 
+
+
+	local firstUtil = targetUtility(first)
+	local secondUtil = targetUtility(second)
+
+	if firstUtil < secondUtil then
+
+		return second
+
+	end
+
+	return first
+
+
+end
+
+
+local function TargetUtility(targetHero)
+
+	local util = 0
+
+	local basePos = core.allyMainBaseStructure:GetPosition()
+
+	local dist = Vector3.Distance2D(basePos, targetHero:GetPosition()) 
+
+	local health = targetHero:GetHealthPercent()
+
+	
+
+	util = util - dist / 500 + (1 - health) * 1.5
+
+	return util
+
+end
+
+
